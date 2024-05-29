64a5f13fde7e5aee33b57e57a45fc394714a8a4c
Marshal10 <marshalmaleane123@gmail.com>
2024-05-29 20:39:39 +0530

    bill and tip components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <BillInput>How much was the bill?</BillInput>
+      <TipPercentage>How did you like the service?</TipPercentage>
+      <TipPercentage>How did your friend like the service?</TipPercentage>
+    </div>
+  );
+}
+
+function BillInput({ children }) {
+  return (
+    <div className="bill">
+      <span>{children}</span>
+      <input type="number"></input>
+    </div>
+  );
+}
+
+function TipPercentage({ children }) {
+  return (
+    <div className="tip">
+      <span>{children}</span>
+      <select>
+        <option value={0}>Dissatisfied(0%)</option>
+        <option value={5}>It was okay(5%)</option>
+        <option value={10}>It was good(10%)</option>
+        <option value={20}>Absolutely amazing!(20%)</option>
+      </select>
     </div>
   );
 }
